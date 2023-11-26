66d0a1ffb693894963484a0e3392cb265d20edad
Manish Kumar Sahu <56619260+t-rex777@users.noreply.github.com>
2023-11-26 23:19:56 +0530

    feat: added seed script (#2)

---
+++ drizzle/0003_lonely_doctor_spectrum.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "expenses" ALTER COLUMN "category_id" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "email" text NOT NULL;
---
+++ src/db/seed.ts
@@ -0,0 +1,56 @@
+import { faker } from '@faker-js/faker';
+import * as dotenv from 'dotenv';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { groups, users, usersOnGroups } from '../schema';
+dotenv.config();
+
+if (!('DATABASE_URL' in process.env)) throw new Error('DATABASE_URL not found on .env.development');
+
+const main = async () => {
+  const client = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const db = drizzle(client);
+  const data1: (typeof users.$inferInsert)[] = [];
+  const data2: (typeof groups.$inferInsert)[] = [];
+
+  for (let i = 0; i < 1; i++) {
+    data1.push({
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      dob: faker.date.birthdate().toDateString(),
+      createdAt: new Date(),
+      currency: 'INR',
+      groupId: 1,
+      image: faker.image.avatar(),
+      id: i + 1,
+    });
+  }
+
+  for (let i = 0; i < 1; i++) {
+    data2.push({
+      name: faker.internet.userName(),
+      createdAt: new Date(),
+      id: i + 1,
+      image: faker.image.avatar(),
+    });
+  }
+
+  console.log('Seed start', data1, data2);
+  // !for deleting db data
+  //   await db.delete(users);
+  //   await db.delete(groups);
+
+  await db.insert(groups).values(data2);
+  await db.insert(users).values(data1);
+  await db.insert(usersOnGroups).values({
+    groupId: data2[0].id,
+    userId: data1[0].id,
+  });
+
+  console.log('Seed done');
+};
+
+main();
